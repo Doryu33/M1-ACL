a439c70907bd32cfa6622b36b90465e01be1bd5e
alex <alexandre.serratore@gmail.com>
2022-10-11 12:27:13 +0200

    Collision monstre -> Joueur

--- terminal/src/entities/Monster.java
+++ terminal/src/entities/Monster.java
@@ -25,19 +25,19 @@ public final class Monster extends Entity {
         Random random = new Random();
         int randomDirection = random.nextInt(4);
         if(randomDirection == 0) {
-            if(actualRoom.isTileWalkable(getX(),getY()-1)){
+            if(actualRoom.isTileWalkable(getX(),getY()-1) && !actualRoom.isPlayerHere(getX(),getY()-1)){
                 setY(getY()-1);
             }
         } else if (randomDirection == 1) {
-            if(actualRoom.isTileWalkable(getX(),getY()+1)){
+            if(actualRoom.isTileWalkable(getX(),getY()+1) && !actualRoom.isPlayerHere(getX(),getY()+1)){
                 setY(getY()+1);
             }
         } else if (randomDirection == 2) {
-            if(actualRoom.isTileWalkable(getX()-1,getY())){
+            if(actualRoom.isTileWalkable(getX()-1,getY()) && !actualRoom.isPlayerHere(getX()-1,getY())){
                 setX(getX()-1);
             }
         } else {
-            if(actualRoom.isTileWalkable(getX()+1,getY())){
+            if(actualRoom.isTileWalkable(getX()+1,getY()) && !actualRoom.isPlayerHere(getX()+1,getY())){
                 setX(getX()+1);
             }
         }
